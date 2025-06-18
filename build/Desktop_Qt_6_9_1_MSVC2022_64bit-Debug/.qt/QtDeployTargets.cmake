889eca40ca1b4ac16a3e76e17bcade54b3658bc9
eayral <ethemali00@gmail.com>
2025-06-18 23:04:28 +0300

    Remove generated build directory

--- build/Desktop_Qt_6_9_1_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_UAVTelemViewer_FILE C:/Users/Ethem Ali/Projects/UAVTelemViewer/build/Desktop_Qt_6_9_1_MSVC2022_64bit-Debug/UAVTelemViewer.exe)
-set(__QT_DEPLOY_TARGET_UAVTelemViewer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_UAVTelemViewer_RUNTIME_DLLS C:/Qt/6.9.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Networkd.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Cored.dll)
